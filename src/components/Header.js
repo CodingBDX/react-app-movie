c1883a0d06585028debf5b84a306fdf81e405b6e
CodingBDX <antonycharier@gmail.com>
2022-06-12 09:42:04 +0000

    react movies tailwindcss

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,27 @@ import { NavLink } from 'react-router-dom'
 export default function Header() {
   return (
     <div className='header'>
-      <nav>
-        <ul>
-       <NavLink to='/' className={(nav) => ( nav.isActive ? 'nav-active' : '')}> <li>Accueil</li></NavLink>
-       <NavLink to='/coup-de-coeur' className={(nav) => ( nav.isActive ? 'nav-active' : '')}> <li>Coup de coeur</li></NavLink>
+      
+      <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+  <div class="flex items-center flex-shrink-0 text-white mr-6">
+    <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+    <span class="font-semibold text-xl tracking-tight">React movies
+</span>
+  </div>
+  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <ul class="text-sm lg:flex-grow">
 
-        </ul>
-      </nav>
-      <h1>React movies</h1>
+       <NavLink to='/' class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4" className={(nav) => ( nav.isActive ? 'nav-active' : '')}> <li>Accueil</li></NavLink>
+       <NavLink to='/coup-de-coeur' class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4" className={(nav) => ( nav.isActive ? 'nav-active' : '')}> <li>Coup de coeur</li></NavLink>
+
+    </ul>
+    <div>
+      <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
+    </div>
+  </div>
+</nav>
+      
+
     </div>
   )
 }
